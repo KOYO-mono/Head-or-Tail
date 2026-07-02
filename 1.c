8201f83243077d399e5ee789d1432b4096bf32e7
KOYO <mononosuke0401@gmail.com>
2026-07-02 20:29:23 +0900

    HoT: initial revision

---
+++ 1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+    int heads = 0;
+    int tails = 0;
+
+    srand(time(NULL));
+
+    printf("Tossing a coin...\n");
+
+    for (int i = 1; i <= 3; i++)
+    {
+        int coin = rand() % 2;
+
+        if (coin == 0)
+        {
+            printf("Round %d: Heads\n", i);
+            heads++;
+        }
+        else
+        {
+            printf("Round %d: Tails\n", i);
+            tails++;
+        }
+    }
+
+    printf("Heads: %d, Tails: %d\n", heads, tails);
+
+    return 0;
+}
